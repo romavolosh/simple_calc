87171d906d2d91d4e2e8c44c910272d854a3da27
Roman Voloshin <romavolosh@gmail.com>
2023-11-13 17:36:46 +0200

    first commit

---
+++ s_calc/s_calc.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+
+	float num1, num2, res;
+
+	std::cout << "Enter num1: ";
+	std::cin >> num1;
+
+	std::cout << "Enter num2: ";
+	std::cin >> num2;
+
+	char math;
+
+	std::cout << "Enter '+', '-', '/', '*': ";
+	std::cin >> math;
+
+	switch (math) {
+	case '+': res = num1 + num2; break;
+	case '-': res = num1 - num2; break;
+	case '/': res = num1 / num2; break;
+	case '*': res = num1 * num2; break;
+	}
+
+	std::cout << "Result: " << res;
+
+	return 0;
+}
